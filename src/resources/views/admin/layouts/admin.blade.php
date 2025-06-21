4304c449c38a29e64b1a9e58ec294e3dd88e560a
walid jlidi <walid.jlidi89@gmail.com>
2025-06-21 13:45:59 +0200

    fix tailwindcss version

--- src/resources/views/admin/layouts/admin.blade.php
+++ src/resources/views/admin/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com/4.0.0"></script>
+    <script src="https://cdn.tailwindcss.com/3.4.16"></script>
     <title>لوحة التحكم</title>
 </head>
 <body class="bg-gray-100">
